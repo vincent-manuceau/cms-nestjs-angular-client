2133dd9dec7d8dd835b625943125f1fc3dd9eebe
Vincent Manuceau <manuceauv@gmail.com>
2022-07-08 17:14:51 +0200

    Handle errors

--- src/app/admin/article.service.ts
+++ src/app/admin/article.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Article } from '../models/article';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
+  private baseURL = 'http://127.0.0.1:3000/articles'
+  private httpHeaders = {
+    headers: new HttpHeaders({'Content-Type':'application/json'})
+  }
 
   constructor(private httpClient: HttpClient) { }
 
   createArticle(article:Article){
-      return this.httpClient.post<Article>('http://127.0.0.1:3000/articles',article);
+      return this.httpClient.post<Article>(this.baseURL,article);
+  }
+
+  deleteArticle(article:Article){
+    const fullURL = `${this.baseURL}/${article._id}`
+    return this.httpClient.delete<Article>(fullURL, this.httpHeaders)
   }
 }
--- src/app/articles/article-summary/article-summary.component.ts
+++ src/app/articles/article-summary/article-summary.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { ArticleService } from 'src/app/admin/article.service';
 import { Article } from 'src/app/models/article';
 
 @Component({
@@ -7,10 +9,33 @@ import { Article } from 'src/app/models/article';
   styleUrls: ['./article-summary.component.css']
 })
 export class ArticleSummaryComponent implements OnInit {
+  isWaitingForServerResponse = false;
+  error = null;
+
   @Input() article!: Article
-  constructor() { }
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit(): void {
+  }
+
+  delete(article:Article){
+    this.isWaitingForServerResponse = true;
+    this.articleService.deleteArticle(article).pipe(
+      catchError(this.handleError)
+    ).subscribe(
+      data => {this.isWaitingForServerResponse = false; this.handleSuccess(data)},
+      err => {this.isWaitingForServerResponse = false; this.handleError(err)}
+    );
+  }
+
+  handleError(err:any){
+    console.log('error',err)
+    this.error = err;
+    return throwError(this.error)
+  }
+
+  handleSuccess(data:any){
+    console.log("Success", data)
   }
 
 }
